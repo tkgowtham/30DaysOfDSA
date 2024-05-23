6690a9ece1a99cf978a6fc207b5bd28cf82d8746
tkgowtham <tkgowthamkamal@gmail.com>
2024-05-23 11:31:40 +0530

    Create 5_Sort_An_Array_Of_012.py
    Dutch National Flag Algorithm to sort 0, 1, 2 with in-place method

---
+++ Day1/5_Sort_An_Array_Of_012.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sortColors(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        low = 0
+        mid = 0
+        high = len(arr)-1
+
+        while high >= mid:
+            if arr[mid] == 0:
+                arr[low], arr[mid] = arr[mid], arr[low]
+                low += 1
+                mid += 1
+            
+            elif arr[mid] == 1:
+                mid += 1
+
+            elif arr[mid] == 2:
+                arr[mid], arr[high] = arr[high], arr[mid]
+                high -= 1
+
+        return arr
+        
