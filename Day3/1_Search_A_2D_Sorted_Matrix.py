1ef3d784eb0ae1d376578f3e38d2d9e479f2e17e
tkgowtham <tkgowthamkamal@gmail.com>
2024-05-25 13:36:44 +0530

    Update 1_Search_A_2D_Sorted_Matrix.py

--- Day3/1_Search_A_2D_Sorted_Matrix.py
+++ Day3/1_Search_A_2D_Sorted_Matrix.py
@@ -7,3 +7,28 @@ class Solution: # O(N*M)
                         return True
                 return False
 
+# Using Binary Search -- doesn't work for all cases -- O(log(M*N))
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+
+        n = len(matrix)
+        m = len(matrix[0])
+
+        low = 0
+        high = n*m - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            row = mid // 2
+            col = mid % 2
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return False
