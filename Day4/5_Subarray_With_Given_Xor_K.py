d3fc9eaef3d7ebf17c1cc274882125e92a282b44
tkgowtham <tkgowthamkamal@gmail.com>
2024-05-26 15:51:04 +0530

    Create 5_Subarray_With_Given_Xor_K.py

---
+++ Day4/5_Subarray_With_Given_Xor_K.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):         
+        xr = 0
+        count = 0
+        dict1 = {0 : 1}
+        for i in range(len(A)):
+            xr = xr ^ A[i]
+            x = xr ^ B
+            count += dict1.get(x, 0)
+            dict1[xr] = dict1.get(xr, 0) + 1
+        
+        return count
