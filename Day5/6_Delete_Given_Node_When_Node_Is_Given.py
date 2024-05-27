2ec9762090f4b1ed4cfa87cbec28f7f63ea130ca
tkgowtham <tkgowthamkamal@gmail.com>
2024-05-27 13:55:57 +0530

    Create 6_Delete_Given_Node_When_Node_Is_Given.py

---
+++ Day5/6_Delete_Given_Node_When_Node_Is_Given.py
@@ -0,0 +1,20 @@
+#In a linked list, we are directly given with node that has to be deleted.
+#So just copy the next one to the current one, and point the current to next next
+#O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        
+        node.val = node.next.val
+        node.next = node.next.next
+        
