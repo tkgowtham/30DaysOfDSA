0b0138dfdc713ae92c671cd377228a806e2d900f
tkgowtham <tkgowthamkamal@gmail.com>
2024-05-28 12:50:03 +0530

    Create 4_Palindrome_Linked_List.py

---
+++ Day6/4_Palindrome_Linked_List.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverse(self, head):
+        temp = head
+        prev = None
+        while temp != None:
+            front = temp.next
+            temp.next = prev
+            prev = temp
+            temp = front
+
+        return prev
+
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while slow != None and fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+
+        slow = self.reverse(slow)
+
+        while head != None and slow != None:
+            if head.val != slow.val:
+                return False
+            slow = slow.next
+            head = head.next
+
+        return True
