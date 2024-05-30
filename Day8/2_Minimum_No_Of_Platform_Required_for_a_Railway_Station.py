ac775138521d1237b136c32217bd6cc78c0b08fa
tkgowtham <tkgowthamkamal@gmail.com>
2024-05-30 12:34:32 +0530

    Create 2_Minimum_No_Of_Platform_Required_for_a_Railway_Station.py

---
+++ Day8/2_Minimum_No_Of_Platform_Required_for_a_Railway_Station.py
@@ -0,0 +1,29 @@
+#TC = O(N) + O(NlogN) SC = O(1)
+
+def minimumPlatform(self,n,arr,dep):
+        # code here
+        #times = [(arr[i], dep[i]) for i in range(n)]
+        #times.sort()
+        #arr = [times[i][0] for i in range(n)]
+        #dep = [times[i][1] for i in range(n)]
+        
+        arr.sort()
+        dep.sort()
+        
+        platforms_needed = max_platforms = 1
+    
+        i = 1
+        j = 0
+        
+        while i < n and j < n:
+            if arr[i] <= dep[j]:
+                platforms_needed += 1
+                i += 1
+            elif arr[i] > dep[j]:
+                platforms_needed -= 1
+                j += 1
+                
+            if platforms_needed > max_platforms:
+                max_platforms = platforms_needed
+        
+        return max_platforms
