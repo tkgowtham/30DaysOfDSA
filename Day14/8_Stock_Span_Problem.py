18d54b57cbb5b209d494032202e80400fc50fd9c
tkgowtham <tkgowthamkamal@gmail.com>
2024-06-05 23:31:35 +0530

    Create 8_Stock_Span_Problem.py

---
+++ Day14/8_Stock_Span_Problem.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = deque()
+
+    def next(self, price: int) -> int:
+        span = 1
+        
+        while self.stack and self.stack[-1][0] <= price:
+            span += self.stack.pop()[1]
+
+        self.stack.append((price, span))
+
+        return span
