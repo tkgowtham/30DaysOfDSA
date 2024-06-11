94b046826f040a1cc263913aa705f2bdec028c37
tkgowtham <tkgowthamkamal@gmail.com>
2024-06-11 15:51:37 +0530

    Create 6_Flatten_Binary_Tree_to_Linked_List.py

---
+++ Day20/6_Flatten_Binary_Tree_to_Linked_List.py
@@ -0,0 +1,29 @@
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if root == None:
+            return
+
+        stack = deque([root])
+
+        while stack:
+            node = stack.pop()
+            if node.right:
+                stack.append(node.right)
+
+            if node.left:
+                stack.append(node.left)
+
+            if stack:
+                node.right = stack[-1]
+
+            node.left = None
