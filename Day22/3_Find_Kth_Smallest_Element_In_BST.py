95106a8ef81a3aa0c77ad4e51d315cb003909bfb
tkgowtham <tkgowthamkamal@gmail.com>
2024-06-13 17:33:56 +0530

    Update and rename 3_Find_Kth_Smallest_Largest_Element_In_BST.py to 3_Find_Kth_Smallest_Element_In_BST.py

--- Day22/3_Find_Kth_Smallest_Element_In_BST.py
+++ Day22/3_Find_Kth_Smallest_Element_In_BST.py
@@ -4,19 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def reverseInorder(self, node, counter, k, kLarge):
-        if node == None or counter[0] >= k:
-            return
-
-        self.reverseInorder(node.right, counter, k, kLarge)
-        counter[0] += 1
-        if counter[0] == k:
-            kLarge[0] = node.val
-            return
-
-        self.reverseInorder(node.left, counter, k, kLarge)
-      
+class Solution:      
     def inorder(self, node, counter, k, kSmall):
         if node == None or counter[0] >= k:
             return
@@ -33,13 +21,8 @@ class Solution:
 
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         kSmall = [float('-inf')]
-        #kLarge = [float('inf')]
         counter = [0]
 
         self.inorder(root, counter, k, kSmall)
-
-        counter[0] = 0
-
-        self.reverseInorder(root, counter, k, kLarge)
 
         return kSmall[0]
