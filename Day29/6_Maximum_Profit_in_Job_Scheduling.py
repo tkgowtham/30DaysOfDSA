dd723e16fe0c18aee9be530c0b57f90231fe1ed7
tkgowtham <tkgowthamkamal@gmail.com>
2024-06-20 21:01:35 +0530

    Create 6_Maximum_Profit_in_Job_Scheduling.py

---
+++ Day29/6_Maximum_Profit_in_Job_Scheduling.py
@@ -0,0 +1,29 @@
+class Solution:
+    
+    #Function to find the maximum profit and the number of jobs done.
+    def JobScheduling(self,Jobs,n):
+        
+        # code here
+        Jobs = sorted(Jobs, key=lambda job: (-job.profit, job.deadline))
+
+        max_deadline = -1
+        for i in Jobs:
+           max_deadline = max(max_deadline, i.deadline)
+           
+        days = [-1] * max_deadline
+            
+        time = 1
+        no_of_job = 0
+        profit = 0
+
+
+        for i in range(n):
+            for j in range(Jobs[i].deadline-1, -1, -1):
+                if days[j] != -1:
+                    continue
+                days[j] = Jobs[i].id
+                no_of_job += 1
+                profit += Jobs[i].profit
+                break
+        
+        return [no_of_job, profit]
