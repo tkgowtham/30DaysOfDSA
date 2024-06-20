d905f9ac1ee5a78c8b866ed21f02cbcb671c2dbe
tkgowtham <tkgowthamkamal@gmail.com>
2024-06-20 09:56:10 +0530

    Create 2_Coin_Change.py

---
+++ Day29/2_Coin_Change.py
@@ -0,0 +1,13 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+        
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] = min(dp[x], dp[x - coin] + 1)
+
+        return dp[amount] if dp[amount] != float('inf') else -1
